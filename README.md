fca00349e8952c1a3d0bbc5b51ca90589f1b7ba5
Daniel Moore <wron2@hotmail.com>
2021-04-06 23:49:53 -0700

    Adding everything together

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# good-readme-generator
+# Good Readme Generator
+
+## Description
+    In this project I was given the assignment of creating a program using node that would accept the users input and, through a series of questions, take the responses and generate a Readme file using correct formatting. <br> This program is to be used as a quick and easy way to create a Readme file for a new project.
+    
+##### Table of Contents
+    [Installation](#installation)
+    [Usage](#usage)
+    [License](#license)
+    [Contributing](#contributing)
+
+<a name="installation"></a>
+## Installation
+    
+        Simply download the application folder and then run the program inside of your terminal, you can even open up the code inside of VisualStudio or whatever code editor to see how I have laid it out.
+
+<a name="usage"></a>
+## Usage
+
+    Here is a link to a video showing the program at work: https://drive.google.com/file/d/114YlTDD7TxoVl14lvKbRSKqVYCKond1y/view <br> Answer the questions as they appear and enjoy your new Readme file!
+
+<a name="license"></a>
+## License
+
+    MIT License <br><br>Copyright (c) [2021] [Daniel Moore] <br><br> Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: <br><br> The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. <br><br> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+<a name="contributing"></a>
+## Contributing
+
+    Contributions are not accepted at the current time however if there are any major problems you find with the program feel free to comment and let me know. Thanks.
---
+++ index.js
@@ -0,0 +1,198 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer.prompt([
+
+    {
+        name: 'Title',
+        type: 'input',
+        message: 'What is the title of your project?'
+
+    },
+    {
+        name: 'Description',
+        type: 'input',
+        message: 'What is the description of your project?'
+
+    },
+    {
+        name: 'InstallationInstructions',
+        type: 'input',
+        message: 'What are the instruction to install your project?'
+
+    },
+    {
+        name: 'UsageInfo',
+        type: 'input',
+        message: 'Explain the usage of your project.'
+
+    },
+    {
+        name: 'Licensing',
+        type: 'input',
+        message: 'What type of License would you like to use?'
+
+    },
+    {
+        name: 'ContributionGuidelines',
+        type: 'input',
+        message: 'What are the guidelines to contributing in your repository?'
+
+    },
+    {
+        name: 'TestInstructions',
+        type: 'input',
+        message: 'What are some instructions related to testing you would like to add?'
+
+    }
+
+]).then(data => {
+
+let titleData = `# ${data.Title}`
+
+let descriptionData = `
+
+## Description
+    ${data.Description}`
+
+let tableOfContentsData;
+let installationData;
+let usageData;
+let licenseData; 
+let contributingData;
+let testsData;   
+let questionsData;
+
+// See if any extra fields have been filled making a table of contents necessary
+if(data.InstallationInstructions === `` && data.UsageInfo === `` && data.License === `` && data.ContributionGuidelines === `` && data.TestInstructions === ``){
+
+    tableOfContentsData = ``;
+    console.log('Shouldnt be a TOC bruh')
+
+}else{
+
+    tableOfContentsData = `
+    
+##### Table of Contents`;
+
+    console.log('We def need a TOC my dude')
+
+}
+
+// Check if there are installations instructions
+if(data.InstallationInstructions != ``){
+
+    installationData = `
+
+<a name="installation"></a>
+## Installation
+    
+        ${data.InstallationInstructions}`;
+
+    tableOfContentsData = tableOfContentsData + `
+    [Installation](#installation)`
+    console.log('installation info')
+
+}else{
+
+    installationData = ``;
+
+}
+
+// See if there is usage info
+if(data.UsageInfo != ``){
+
+    usageData = `
+
+<a name="usage"></a>
+## Usage
+
+    ${data.UsageInfo}`;
+
+    tableOfContentsData = tableOfContentsData + `
+    [Usage](#usage)`
+    console.log('usage info')
+
+}else{
+
+    usageData = ``;
+
+}
+
+// seeing if there is license data
+if(data.Licensing != ``){
+
+    licenseData = `
+
+<a name="license"></a>
+## License
+
+    ${data.Licensing}`;
+
+    tableOfContentsData = tableOfContentsData + `
+    [License](#license)`
+    console.log('license info')
+
+}else{
+
+    licenseData = ``;
+
+}
+
+// Checking for contribution guidelines
+if(data.ContributionGuidelines != ``){
+
+    contributingData =`
+
+<a name="contributing"></a>
+## Contributing
+
+    ${data.ContributionGuidelines}`;
+
+    tableOfContentsData = tableOfContentsData + `
+    [Contributing](#contributing)`
+    console.log('contributing info')
+
+}else{
+
+    contributingData = ``;
+
+}
+
+// Checking for test instructions
+if(data.TestInstructions != ``){
+
+    testsData = `
+
+<a name="tests"></a>
+## Tests
+
+    ${data.TestInstructions}`;
+
+    tableOfContentsData = tableOfContentsData + `
+    [Tests](#tests)`
+    console.log('tests info')
+
+}else{
+
+    testsData = ``;
+
+}
+
+// Putting everything together
+theData = titleData + descriptionData + tableOfContentsData + installationData + usageData + licenseData + contributingData + testsData;
+
+    // Writing the put together data to the readme
+    fs.writeFile('./README.md' , theData, function(err){
+
+        if (err) return console.log(err);
+        console.log('Hello World > helloworld.txt');
+
+    });
+
+// Error catching
+}).catch(err => {
+
+    console.log(err);
+
+})
